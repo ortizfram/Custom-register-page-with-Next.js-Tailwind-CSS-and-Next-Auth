1d205973b29d7bdcddd97558fc9c0ae95f63e9e3
melon <ortizfranco48@gmail.com>
2023-03-31 20:05:43 -0300

    API route and env vars

---
+++ nextauth/pages/api/auth/[...nextauth].js
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth/next";
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+});
